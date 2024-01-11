84ac6c0ea9ec5b6e9480ffb6c0671430a5b9cafd
Noah Nininger <114418869+NoahNininger@users.noreply.github.com>
2024-01-11 10:53:21 -0500

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,9 @@
+/* 
+   This is a multiple-line comment
+   A signle-line comment can be
+   achieved by using '//', but since
+   this is a multiple-line comment,
+   we use the forward slash and star
+   as such...
+*/
+console.log('Noah Nininger');   // output name into the console
